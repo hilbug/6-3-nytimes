00308f888a471aef2222acaafd5a509e4e9791b9
a-thread <aiden.threadgoode@gmail.com>
2020-07-25 13:20:46 -0400

    forms added

--- assets/script.js
+++ assets/script.js
@@ -6,8 +6,11 @@ $(document).ready(function(){
     const endYear = $.trim($("#endYearInput").val());
     const searchBtn = $("#searchBtn");
     const clearBtn = $("#clearBtn");
+    const apiKey = "WFDrEXAvwlYsmHSYDpM9CazUe3KFYeR9"
 
     // construct query URL
+    var queryUrlBase = "http://api.nytimes.com/svc/search/v2/articlesearch.json?" + "api-key=" + apiKey
+
 
     // code for AJAX
     
